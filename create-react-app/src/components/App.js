46e5ac63c6959036da7370b11987227695ef504f
Samaelish <semnd007@gmail.com>
2021-03-23 20:00:02 +0300

    foundation

--- create-react-app/src/components/App.js
+++ create-react-app/src/components/App.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import CountButton from './CountButton'
+import React, { useState, useEffect } from 'react'
+import CountButton from './CountButton/CountButton'
+import SearchBar from './SearchBar/SearchBar'
+import Button from './Button/Button'
 
 const App = () => {
+    const [productsState, setProductsState] = useState([])
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.herokuapp.com/products/')
+            .then(res=>res.json())
+            .then(productsArray=> {
+                const newProductsState = productsArray.map((product) => {
+                    return product.title
+                })
+                setProductsState(newProductsState)
+            })
+
+    }, [])
+
+    const hasProducts = productsState.length > 0
+
     return (
         <div>
-            <CountButton incrementBy={1} buttonColor={'blue'} borderRadius={'2px'}/>
-            <CountButton incrementBy={5} buttonColor={'red'} borderRadius={'100px'}/>
-            <CountButton incrementBy={800} buttonColor={'green'} borderRadius={'50%'}/>
+            <Button>Hello World</Button>
+            <Button buttonText='hello'/>
+            <Button buttonText='otherButton'/>
+            <Button buttonText='lalala'/>
+            {/* <CountButton incrementBy={1} buttonColor={'blue'}/> */}
+            
+
+            {/* Conditional rendering */}
+            {/* {hasProducts ? <SearchBar products={productsState} /> : 'Loading...'} */}
         </div>
     )
 }
